74f9b6d21f678beb8ea65693fd0e7ca2e4bccbf8
Roberto <j.roberto.garcia@gmail.com>
2025-11-23 12:34:42 -0600

    Actualizacion de CSS

--- views/layout.php
+++ views/layout.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medicos Oaxaca</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="/build/css/app.css">
+    <link rel="stylesheet" href="/build/css/medicos.css">
     <link rel="stylesheet" href="/build/css/formularios_medicos.css">
 
 </head>
